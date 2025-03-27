fadb2371687930b232aeab5b51a4bb31177a33b6
racionalVol1 <guilhermeperondi09@gmail.com>
2025-03-27 10:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ LogicaSoundFY/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Bem vindo ao SoundFY");
+Console.WriteLine("\n");
+
+void Menu()
+{
+    Console.WriteLine("1 - Entrar como ADM");
+    Console.WriteLine("2 - Entrar como ouvinte");
+    Console.WriteLine("3 - Sair");
+    Console.WriteLine("\n");
+    Console.Write("Digite uma opção: ");
+
+    int opcao = int.Parse(Console.ReadLine());
+    Console.WriteLine(
+}
+
+
+
+
+
+
+
